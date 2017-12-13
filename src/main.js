7633706b49444ca41b0462077fc591f39dcac75f
HowardTangHw <2379034@qq.com>
2017-12-13 14:16:19 +0800

    DouyuTool init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import router from './router';
+Vue.use(ElementUI);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -14,4 +15,4 @@ new Vue({
   router,
   template: '<App/>',
   components: { App }
-})
+});
